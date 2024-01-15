7654d2af76a08bd7e17db368e7ba802dadb08e31
Krzysztof Kalinowski <krzykalinowski@gmail.com>
2024-01-15 21:22:51 +0100

    added tailwind styling

---
+++ src/scripts/main.js
@@ -0,0 +1,4 @@
+const copyButton = document.querySelector("button:has(.bg-copy)");
+const loadButton = document.querySelector("button:has(.bg-load)");
+const deleteButton = document.querySelector("button:has(.bg-delete)");
+const textContainer = document.querySelector("textarea");
