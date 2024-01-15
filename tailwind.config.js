c2f1fae363dee3a995d28c6c0e2dffcbacde6ea6
Krzysztof Kalinowski <krzykalinowski@gmail.com>
2024-01-15 21:21:22 +0100

    added html markup

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,13 @@ module.exports = {
     "./src/**/*.{html,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'delete': "url('img/delete-icon.png')",
+        'load': "url('img/load-icon.png')",
+        'copy': "url('img/copy-icon.png')"
+      }
+    },
   },
   plugins: [],
 }
